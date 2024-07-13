6b6f1fce7d600237af6e159cb26e1a66278c965c
The-Developer-Sai <140163513+The-Developer-Sai@users.noreply.github.com>
2024-07-13 21:41:48 +0530

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,239 @@
+
+from pathlib import Path
+
+# Django environ
+
+import environ
+
+env = environ.Env()
+
+environ.Env.read_env()
+
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-jm1*6=p$)2k#(f*(yn-i!&7hr8x%)pgsxhz*r@xs1!)qp*p6to'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'secureapp',
+
+    'crispy_forms',
+    'crispy_bootstrap5',
+
+    'django_recaptcha',
+
+    'django_otp',
+    'django_otp.plugins.otp_static',
+    'django_otp.plugins.otp_totp',
+    'two_factor',
+
+    'axes', # Axes
+
+]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
+# Recaptcha keys
+
+RECAPTCHA_PUBLIC_KEY = '6Ld0q4MpAAAAAIxuAqaTs5LbhvzLnWPq8E1Bvn6e'
+RECAPTCHA_PRIVATE_KEY = '6Ld0q4MpAAAAAIvGV3dxHd1ujVI03iT6AVocJiGC'
+
+# 2FA
+
+LOGIN_URL = 'two_factor:login'
+LOGIN_REDIRECT_URL = 'dashboard'
+
+
+AUTHENTICATION_BACKENDS = [
+    # AxesStandaloneBackend should be the first backend in the AUTHENTICATION_BACKENDS list.
+    'axes.backends.AxesStandaloneBackend',
+
+    # Django ModelBackend is the default authentication backend.
+    'django.contrib.auth.backends.ModelBackend',
+]
+
+
+
+
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+
+    'django_otp.middleware.OTPMiddleware', #2FA
+
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'django_auto_logout.middleware.auto_logout', # Auto logout
+
+    'axes.middleware.AxesMiddleware', # Axes
+]
+
+ROOT_URLCONF = 'webchecklist.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+
+                'django_auto_logout.context_processors.auto_logout_client', # Auto logout
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'webchecklist.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.0/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'Asia/Kolkata'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Auto logout
+
+from datetime import timedelta
+
+
+AUTO_LOGOUT = {
+    'IDLE_TIME': 15,
+    'REDIRECT_TO_LOGIN_IMMEDIATELY': True,
+    'MESSAGE': 'The session has expired. Please login again to continue.',
+}
+
+
+# Axes configuration
+
+#AXES_FAILURE_LIMIT: 3 # How many times a user can fail a login
+
+#AXES_COOLOFF_TIME: 2 # Wait 2 hours before attempting to login again
+
+#AXES_RESET_ON_SUCCESS = True # Reset failed login attempts
+
+AXES_LOCKOUT_TEMPLATE = 'account-locked.html' # Add a custom template
+
+# SMTP configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = 'True'
+
+EMAIL_HOST_USER = '' # - GMAIL email address
+EMAIL_HOST_PASSWORD = '' # - APP password
+
+DEFAULT_FROM_EMAIL = '' # - GMAIL email address
+
+
+# Pre-deployment security options
+
+'''
+# CSRF protection
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIES_SECURE = True
+
+# SSL redirect
+
+SECURE_SSL_REDIRECT = True
+
+# Enable HSTS
+
+SECURE_HSTS_SECONDS = 86400
+SECURE_HSTS_PRELOAD = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+'''
+
+# Look into a CSP (Content Security Policy)
+
+
+
+
+
+
+
+
+
